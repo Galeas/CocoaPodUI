516dd3c078ff53582cdcdb5ad0847289c1f7404a
Galeas <dacassar1987@gmail.com>
2014-03-20 18:51:39 +0200

    MIT Licence text added

--- CocoaPodUI/Categories/NSWindow+Sizing.m
+++ CocoaPodUI/Categories/NSWindow+Sizing.m
@@ -5,6 +5,25 @@
 //  Created by Evgeniy Kratko on 01.03.14.
 //  Copyright (c) 2014 akki. All rights reserved.
 //
+//Copyright (c) 2014 Yevgeniy Branitsky (Kratko)
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
 
 #import "NSWindow+Sizing.h"
 
@@ -23,10 +42,6 @@
     
     frameW.size.width = frameC.size.width ;
     frameW.size.height = newHeight ;
-    // Since window origin is at the bottom, and we want
-    // the bottom to move instead of the top, we also
-    // adjust the origin.y.  However, since screen y is
-    // measured from the top, we must subtract instead of add
     frameW.origin.y -= dY ;
     
     [self setFrame:frameW display:display] ;
